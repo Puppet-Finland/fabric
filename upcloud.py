e0d3d79e6d8ff02ea7d2460d0740f3a4dae7d7d2
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-09-04 17:23:17 +0300

    Moved upcloud imports inside upcloud.bootstrap task
    The task hierarchy logic placed all tasks in utils.py under upcloud in the
    hierarchy. It seems this was due to utils being later in sorting order than
    upcloud. Moving the imports into the task itself works around this issue.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- upcloud.py
+++ upcloud.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 from fabric.api import *
 
-from . import util, puppet
-
 @task
 def bootstrap(puppetmaster=''):
     """Bootstrap an Ubuntu 14.04 host on Upcloud"""
+    from . import util, puppet
+
     util.install_sudo()
 
     fqdn = run('hostname -f')
