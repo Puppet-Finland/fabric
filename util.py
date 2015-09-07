fc64db738623b9d20b9a7cedb7396690e7f37f7e
Samuli Seppänen <samuli@openvpn.net>
2015-09-07 17:03:04 +0300

    Added util.add_host_entry
    This task allows adding entries to /etc/hosts
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- util.py
+++ util.py
@@ -30,3 +30,15 @@ def install_sudo():
     with settings(warn_only=True):
         if run("which sudo").failed:
             run(vars.os.package_install_cmd % "sudo")
+
+@task
+def add_host_entry(ip, hostname, domain):
+    """Add an entry to /etc/hosts"""
+    host_line = ip+" "+hostname+"."+domain+" "+hostname
+
+    # Only add entry if it does not exist already. We don't want warnings about
+    # grep not finding the entry, as that's to be expected.
+    with hide("warnings"):
+        if run("grep \""+host_line+"\" /etc/hosts").failed:
+            sudo("echo "+host_line+" >> /etc/hosts")
+
