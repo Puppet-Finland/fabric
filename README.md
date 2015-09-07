969090b087a81000500200ab6248f5513a247e07
Samuli Seppänen <samuli@openvpn.net>
2015-09-07 16:55:23 +0300

    Added basic package and service management tasks
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # fabric
+
 Generic Fabric scripts to bootstrap and manage Puppet nodes
+
+# Bugs
+
+## Imports need to be inside defs
+
+If imports are placed at the top of the Python files, the task hierarchy gets 
+messed up. See message for commit e0d3d79 for details.
+
+## Operating system variables are constantly redefined
+
+Currently each method has to individually (re)determine the OS-specific 
+variables. This is suboptimal if a task calls many methods which also need to 
+runnable by themselves, i.e. Fabric tasks.
--- vars.py
+++ vars.py
@@ -5,7 +5,6 @@ class Vars:
     """Manage parameters based on the operating system"""
 
     def __init__(self, *args, **kwargs):
-
         # Map release files to operating system names and operating system
         # families. This approach has been adapted from
         # facter/operatingsystem/linux.rb and would be redundant _if_ every OS
@@ -16,12 +15,12 @@ class Vars:
         self.release_files = { '/etc/fedora-release': { 'osfamily': 'RedHat' },
                                '/etc/redhat-release': { 'osfamily': 'RedHat' } }
 
-        osfamily = self.get_osfamily()
-        lsbdistcodename = self.get_lsbdistcodename()
+        self.osfamily = self.get_osfamily()
+        self.lsbdistcodename = self.get_lsbdistcodename()
 
         # Populate OS-specific parameters
-        if osfamily == 'Debian': self.os = Debian(osfamily, lsbdistcodename)
-        elif osfamily == 'RedHat': self.os = RedHat(osfamily, lsbdistcodename)
+        if self.osfamily == 'Debian': self.os = Debian()
+        elif self.osfamily == 'RedHat': self.os = RedHat()
 
     def get_osfamily(self):
         """Detect operating system family"""
@@ -61,28 +60,49 @@ class Vars:
 
 class Linux(object):
     """Parameters for Linux-based operating systems"""
-    def __init__(self, osfamily, lsbdistcodename, *args, **kwargs):
-        self.osfamily = osfamily
-        self.lsbdistcodename = lsbdistcodename
+    def __init__(self, *args, **kwargs):
+        """Currently empty"""
+        if self.has_systemd():
+            self.service_start_cmd = "systemctl start %s"
+            self.service_stop_cmd = "systemctl stop %s"
+            self.service_restart_cmd = "systemctl restart %s"
+            self.service_status_cmd = "systemctl status %s"
+        else:
+            self.service_start_cmd = "service %s start"
+            self.service_stop_cmd = "service %s stop"
+            self.service_restart_cmd = "service %s restart"
+            self.service_status_cmd = "service %s status"
+
+    def has_systemd(self):
+        """Determine if the operating system uses systemd"""
+        with hide("everything"), settings(warn_only=True):
+            if run("which systemctl").succeeded:
+                return True
+            else:
+                return False
 
 class RedHat(Linux):
     """Parameters for RedHat-based operating systems"""
-    def __init__(self, osfamily, lsbdistcodename, *args, **kwargs):
-        super(RedHat, self).__init__(osfamily, lsbdistcodename)
+    def __init__(self, *args, **kwargs):
+        super(RedHat, self).__init__()
         self.package_refresh_cmd = "yum makecache"
         self.package_upgrade_cmd = "yum update"
         self.package_install_cmd = "yum install %s"
+        self.package_local_install_cmd = "rpm -i %s"
 
 class Debian(Linux):
     """Parameters for Debian-based operating systems"""
-    def __init__(self, osfamily, lsbdistcodename, *args, **kwargs):
-        super(Debian, self).__init__(osfamily, lsbdistcodename)
+    def __init__(self, *args, **kwargs):
+        super(Debian, self).__init__()
         self.package_refresh_cmd = "apt-get update"
         self.package_upgrade_cmd = "apt-get dist-upgrade"
         self.package_install_cmd = "apt-get install %s"
+        self.package_autoremove_cmd = "apt-get autoremove"
+        self.package_local_install_cmd = "dpkg -i %s"
+        # Using %s after the single quotes does not seem to work
+        self.package_installed_cmd = "dpkg-query -Wf'${db:Status-abbrev}'"
 
 class Trusty(Debian):
     """Parameters for Ubuntu Trusty (14.04)"""
-    def __init__(self, osfamily, lsbdistcodename, *args, **kwargs):
-        super(Trusty, self).__init__(osfamily, lsbdistcodename)
-
+    def __init__(self, *args, **kwargs):
+        super(Trusty, self).__init__()
