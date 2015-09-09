91ff843a52b54336d159c9b49a2cc93b60fc2aa8
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-09-09 15:09:31 +0300

    Added git.clone task for cloning and adding submodules
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

---
+++ git.py
@@ -0,0 +1,18 @@
+from fabric.api import *
+
+@task
+def clone(dir, url, reponame=None, submodule=False):
+    """Clone a Git repository as-is or as a submodule"""
+    if not reponame:
+        temp = re.split("/", url)[-1:][0]
+        name = re.split("\.", temp)[0]
+    else:
+        name = reponame
+
+    with cd(dir):
+        if not exists(name):
+            if submodule:
+                cmd = "git submodule add"
+            else:
+                cmd = "git clone"
+            sudo(cmd+" "+url+" "+name)
