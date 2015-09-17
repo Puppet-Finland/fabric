03bc9c74d5f1ba151f5991b596c172c3c961919b
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-09-17 11:14:44 +0300

    Added git.add_submodule and git.add_submodule tasks
    The former adds one submodule with given parameters, whereas the latter reads
    submodule "<url> [<name>]" pairs for a file and adds all as submodules.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- git.py
+++ git.py
@@ -1,4 +1,6 @@
 from fabric.api import *
+from fabric.contrib.files import exists
+import re
 
 @task
 def install():
@@ -23,3 +25,36 @@ def clone(dir, url, reponame=None, submodule=False):
             else:
                 cmd = "git clone"
             sudo(cmd+" "+url+" "+name)
+
+@task
+def add_submodule(url, basedir, name=None):
+    """Add a Git submodule to a directory"""
+    if not name:
+        # This can handle URL in this format:
+        #
+        # https://github.com/Puppet-Finland/postgresql.git
+        # https://github.com/Puppet-Finland/postgresql
+        #
+        stripped = re.sub("\.git$", "", url)
+        name = re.split("/", stripped)[-1:][0]
+        cmd = "git submodule add "+url
+    else:
+        cmd = "git submodule add "+url+" "+name
+
+    with cd(basedir):
+        if not exists(name):
+            sudo(cmd)
+
+@task
+def add_submodules(basedir, file="files/git-submodules"):
+    """Add submodules from url / name pairs listed in a file"""
+    with open(file) as modulelist:
+        for line in modulelist:
+            columns=line.split(" ")
+            url=columns[0].rstrip("\n")
+            if len(columns) > 1:
+                name = columns[1].rstrip("\n")
+            else:
+                name = None
+
+            add_submodule(url, basedir, name)
