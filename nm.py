2fc437c6e30945dc338d42bb5572e239b8e31aa5
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-12-15 13:10:44 +0200

    Added never_default parameter to nm.add_static_connection
    NetworkManager will, by default, create default routes for each new connection.
    This can cause connectivity issues if the new interface is never meant to be
    used as a default gateway, and it gets ranked higher than the real gateway
    interface. This new parameter allows changing this default behavior.
    
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- nm.py
+++ nm.py
@@ -2,13 +2,14 @@
 from fabric.api import *
 
 @task
-def add_static_connection(iface, ip, nm, gw, dns):
+def add_static_connection(iface, ip, nm, gw, dns, never_default="TRUE"):
     """Add a static, secondary ethernet interface"""
     cmd = "nmcli connection"
     sudo("%s modify %s ipv4.addresses %s/%s" % (cmd, iface, ip, nm))
     sudo("%s modify %s ipv4.gateway %s" % (cmd, iface, gw))
     sudo("%s modify %s ipv4.dns %s" % (cmd, iface, dns))
     sudo("%s modify %s ipv4.method manual" % (cmd, iface))
+    sudo("%s modify %s ipv4.never-default %s" % (cmd, iface, never_default))
     sudo("%s modify %s connection.autoconnect yes" % (cmd, iface))
     sudo("%s up %s" % (cmd, iface))
 
