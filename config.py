200a88f49a911c84d6a598d379a818522c6d6aef
Samuli Seppänen <samuli@openvpn.net>
2017-06-19 15:17:00 +0300

    Allow opening arbitrary settings files, while defaulting to settings.ini
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import ConfigParser
 
-def get(section, key):
-    """Get a key's value from settings.ini"""
+def get(section, key, inifile="settings.ini"):
+    """Get a key's value from a inifile"""
     config = ConfigParser.RawConfigParser()
-    config.readfp(open("settings.ini"))
+    config.readfp(open(inifile))
     value = config.get(section, key)
     return value
