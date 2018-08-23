d247fd757ea32a2a4e1d07a1bd376b54f1a97145
Samuli Seppänen <samuli@openvpn.net>
2018-08-23 20:58:22 +0300

    Remove Puppet 4 agent setup tasks
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- puppet.py
+++ puppet.py
@@ -78,66 +78,6 @@ def install_puppetlabs_release_package(pc, proxy_url=None):
         package.download_and_install(url, "puppetlabs-release-pc"+pc, proxy_url=proxy_url)
 
 @task
-def setup_agent4_minimal(pc="1", agent_conf="files/puppet-agent.conf", puppetserver=None, proxy_url=None):
-    """Do the absolute minimum to setup a Puppet Agent"""
-    import package, util, config
-
-    install_puppetlabs_release_package(pc, proxy_url=proxy_url)
-    package.install("puppet-agent")
-
-    # Use puppetserver value from setting.ini file if none is given on the
-    # command-line. If that fails use the default.
-    if not puppetserver:
-        try:    puppetserver = config.get("puppet", "puppetserver")
-        except: puppetserver = None
-
-    # Add a customized puppet.conf
-    util.put_and_chown(agent_conf, "/etc/puppetlabs/puppet/puppet.conf")
-    if puppetserver: server = puppetserver
-    else:            server = "puppet.%s" % domain
-    sudo("puppet config set --section agent server %s" % server)
-
-    util.add_to_path("/opt/puppetlabs/bin")
-
-    run_agent(noop="True", onlychanges="False")
-
-
-@task
-def setup_agent4(hostname=None, domain=None, pc="1", agent_conf="files/puppet-agent.conf", puppetserver=None, proxy_url=None, hosts_file=None):
-    """Setup Puppet 4 agent"""
-    import package, util, config
-
-    if not hostname:
-        hostname = util.get_hostname()
-    if not domain:
-        domain = util.get_domain()
-
-    install_puppetlabs_release_package(pc, proxy_url=proxy_url)
-    package.install("puppet-agent")
-
-    # Use puppetserver value from setting.ini file if none is given on the
-    # command-line. If that fails use the default.
-    if not puppetserver:
-        try:    puppetserver = config.get("puppet", "puppetserver")
-        except: puppetserver = None
-
-    # Add a customized puppet.conf
-    util.put_and_chown(agent_conf, "/etc/puppetlabs/puppet/puppet.conf")
-    if puppetserver: server = puppetserver
-    else:            server = "puppet.%s" % domain
-    sudo("puppet config set --section agent server %s" % server)
-
-    util.set_hostname(hostname + "." + domain)
-    util.add_host_entry(util.get_ip(), hostname, domain)
-
-    # Optionally add hosts from a separate file. This is useful when the IP of
-    # the puppetmaster as seen from the Puppet agent node does not match its
-    # name in DNS.
-    util.add_host_entries(hosts_file)
-    util.add_to_path("/opt/puppetlabs/bin")
-    run_agent(noop="False", onlychanges="False")
-
-@task
 def setup_server4(hostname=None, domain=None, pc="1", forge_modules=["puppetlabs/stdlib", "puppetlabs/concat", "puppetlabs/firewall", "puppetlabs/apt"]):
     """Setup Puppet 4 server"""
     import package, util, git, service
